5766734b40a966147c4222edc1b27ee7c30777a4
Dan <dvf5104@gmail.com>
2016-02-02 22:51:20 -0500

    adding data logic, in progress

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,2 +1,9 @@
 // define our app and dependencies
 var app = angular.module("workout", ["ui.router"]);
+
+app.controller('List.controler', function($scope, $http) {
+  $http.get('data.json')
+       .then(function(res){
+          $scope.data = res.data;                
+        });
+});
